37d736e49a5171208b9355d2977ea31e170446c2
mikeyr013 <mericha4@asu.edu>
2020-06-21 21:50:38 -0700

    Got login code working

--- imageboard/views.py
+++ imageboard/views.py
@@ -7,14 +7,11 @@ def generate_server_nonce():
     return str(urandom(16))
 
 def index(request):
-    greeting = ''
     if 'nonce' in request.session:
         del request.session['nonce']
-    if 'username' in request.session:
-        greeting = "Hi, " + request.session['username']
     else:
         request.session.set_expiry(0)
-    return render(request, 'index.html', {"greeting": greeting})
+    return render(request, 'index.html')
 
 def invalidSignupRequest(request):
     nonce = generate_server_nonce()
@@ -22,12 +19,23 @@ def invalidSignupRequest(request):
     request.session.set_expiry(0)
     return render(request, 'signup.html', {'nonce': nonce, 'errortext': "Something went wrong. Please try again."})
 
+def invalidLoginRequest(request):
+    nonce = generate_server_nonce()
+    request.session['nonce'] = nonce
+    request.session.set_expiry(0)
+    return render(request, 'login.html', {'nonce': nonce, 'errortext': "Something went wrong. Please try again."})
+
+def invalidLoginCredentials(request):
+    nonce = generate_server_nonce()
+    request.session['nonce'] = nonce
+    request.session.set_expiry(0)
+    return render(request, 'login.html', {'nonce': nonce, 'errortext': "Incorrect username or password."})
+
 def takenUsername(request):
     nonce = generate_server_nonce()
     request.session['nonce'] = nonce
     request.session.set_expiry(0)
     return render(request, 'signup.html', {'nonce': nonce, 'errortext': "That username is already taken."})
-
 
 def takenEmail(request):
     nonce = generate_server_nonce()
@@ -45,7 +53,28 @@ def login(request):
             request.session.set_expiry(0)
             return render(request, 'login.html', {'nonce': nonce})
     else:
-        return render(request, 'loggedIn.html')
+        if not ('nonce' in request.session and 'cnonce' in request.POST
+                and 'hashpass' in request.POST and 'username' in request.POST):
+            return invalidLoginRequest(request)
+        else:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT id, username, password, permissions, email FROM users WHERE username = %s", [request.POST['username']])
+                row = cursor.fetchone()
+                if(row is None):
+                    return invalidLoginCredentials(request)
+                else:
+                    passHash = hashlib.sha512((row[2] + request.session['nonce'] + request.POST['cnonce']).encode("utf-8")).hexdigest()
+                    if(not passHash == request.POST['hashpass']):
+                        return invalidLoginCredentials(request)
+                    else:
+                        request.session['userID'] = row[0]
+                        request.session['username'] = row[1]
+                        request.session['permissions'] = row[3]
+                        request.session['email'] = row[4]
+                        if('remember' in request.POST):
+                            request.session.set_expiry(604800)
+                        request.session.cycle_key()
+                        return render(request, 'loggedIn.html')
 
 def signup(request):
     if request.method == "GET":
@@ -73,7 +102,7 @@ def signup(request):
                     if(row is not None):
                         return takenEmail(request)
                     else:
-                        cursor.execute("INSERT INTO users(username, email, password) VALUES (%s, %s, %s)", [request.POST['username'], request.POST['email'], hashlib.sha512(request.POST['hashpass'].encode("utf-8"))])
+                        cursor.execute("INSERT INTO users(username, email, password) VALUES (%s, %s, %s)", [request.POST['username'], request.POST['email'], hashlib.sha512(request.POST['hashpass'].encode("utf-8")).hexdigest()])
                         cursor.execute("SELECT id, username, permissions, email FROM users WHERE username = %s", [request.POST['username']])
                         row = cursor.fetchone()
                         request.session['userID'] = row[0]
